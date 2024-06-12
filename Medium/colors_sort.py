a979242db788e80b20c26c4e6eb2d12f32e17c9a
Hafiz <haafizshamnad@gmail.com>
2024-06-13 00:09:06 +0530

    LeetCode Day 5

---
+++ Medium/colors_sort.py
@@ -0,0 +1,29 @@
+'''
+Sort Colors
+
+Given an array nums with n objects colored red, white, or blue, 
+sort them in-place so that objects of the same color are adjacent, 
+with the colors in the order red, white, and blue.
+
+We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
+
+You must solve this problem without using the library's sort function.
+
+ 
+
+Example 1:
+
+Input: nums = [2,0,2,1,1,0]
+Output: [0,0,1,1,2,2]
+Example 2:
+
+Input: nums = [2,0,1]
+Output: [0,1,2]
+'''
+
+nums = [2,0,2,1,1,0]
+for i in range(0,len(nums)-1):
+    for j in range(0,len(nums)-i-1):
+        if nums[j]>nums[j+1]:
+           nums[j+1], nums[j] = nums[j] , nums[j+1]
+print(nums)
